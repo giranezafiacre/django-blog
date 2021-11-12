66232d31d2ae114e9d8efdf9e9c8b1d0b32999df
unknown <fiacregiraneza@gmail.com>
2021-11-12 11:50:46 +0200

    adding default image

--- blog/models.py
+++ blog/models.py
@@ -8,12 +8,12 @@ import datetime
 class Post(models.Model):
     title = models.CharField(max_length=200,null=False)
     content = models.TextField(max_length=3000, help_text='Enter a content of the a post')
-    image = models.ImageField(upload_to='uploads')
+    image = models.ImageField(upload_to='uploads', default='../static/img/3.jpg')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,verbose_name='created_by')
     created_on = models.DateField(default=datetime.date.today)
     
     class Meta:
-        ordering = ['created_on']
+        ordering = ['-id']
 
     def __str__(self):
         """String for representing the Model object."""
@@ -26,6 +26,10 @@ class Comment(models.Model):
     content = models.TextField(max_length=2000, help_text='Enter your comment')
     post = models.ForeignKey('Post', on_delete=models.SET_NULL, null=True,verbose_name='commented_on')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,verbose_name='commented_by')
+
+    class Meta:
+        ordering = ['-id']
+        
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.content})'
--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -79,27 +79,27 @@ WSGI_APPLICATION = 'djangoblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-#         'NAME': 'blog',
-
-#         'USER': 'postgres',
-
-#         'PASSWORD': 'nyagatoma',
-
-#         'HOST': 'localhost',
-
-#         'PORT': '5432',
-
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'blog',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'nyagatoma',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 # Password validation
